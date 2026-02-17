577241600edb14030fce0fb294e21e8efda628c4
manju1103 <manjunathg820@gmail.com>
2026-02-17 20:49:12 +0530

    Add files via upload

---
+++ studentdata.py
@@ -0,0 +1,13 @@
+Name= input("enter the student name:")
+Roll_number=input("enter the roll number:")
+Section=input("enter the section:")
+Semester=input("enter the semester:")
+College=input("enter the college name:")
+
+print(" \n STUDENT DATA ")
+
+print("Name:",Name)
+print("Roll number:",Roll_number)
+print("Section:",Section)
+print("Semester:",Semester)     
+print("College:",College)
